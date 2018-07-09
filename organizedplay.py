22339309bc7b5dd512616c6b200fe22db52312f0
niuyuxin <niuxinn@126.com>
2018-07-09 21:30:12 +0800

    finished plays rename

--- organizedplay.py
+++ organizedplay.py
@@ -27,15 +27,15 @@ class ListWidget(QListWidget):
     def contextMenuEvent(self, *args, **kwargs):
         menu = QMenu(self)
         if isinstance(self.itemAt(self.mapFromGlobal(QCursor.pos())), QListWidgetItem):
-            # action = QAction(self.tr("删除"), self)
-            # action.triggered.connect(self.deletedAction)
-            # menu.addAction(action)
-            # action = QAction(self.tr("修改"), self)
-            # action.triggered.connect(self.modifiedAction)
-            # menu.addAction(action)
-            # action = QAction(self.tr("激活"), self)
-            # action.triggered.connect(self.activedAction)
-            # menu.addAction(action)
+            action = QAction(self.tr("删除"), self)
+            action.triggered.connect(self.deletedAction)
+            menu.addAction(action)
+            action = QAction(self.tr("修改"), self)
+            action.triggered.connect(self.modifiedAction)
+            menu.addAction(action)
+            action = QAction(self.tr("激活"), self)
+            action.triggered.connect(self.activedAction)
+            menu.addAction(action)
             action = QAction(self.tr("重命名"), self)
             action.triggered.connect(self.renamedAction)
             menu.addAction(action)
@@ -48,6 +48,7 @@ class OrganizedPlay(QDialog, ui_organizedplaydialog.Ui_organizedPlayDialog):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setupUi(self)
+        self.returnPushButton.setDefault(True)
         # add plays
         self.contentLayout = QHBoxLayout()
         self.contentListWidget = ListWidget()
@@ -61,6 +62,7 @@ class OrganizedPlay(QDialog, ui_organizedplaydialog.Ui_organizedPlayDialog):
             item = QListWidgetItem()
             item.setText("testFile{}".format(i))
             item.setIcon(icon)
+            item.setFlags(item.flags() | Qt.ItemIsEditable)
             self.contentListWidget.addItem(item)
         self.contentFrame.setLayout(self.contentLayout)
         # push button clicked and slots
@@ -71,6 +73,7 @@ class OrganizedPlay(QDialog, ui_organizedplaydialog.Ui_organizedPlayDialog):
         self.exportPushButton.clicked.connect(self.onExportPushButtonClicked)
         self.activePushButton.clicked.connect(self.onActivePushButtonClicked)
         # content list widget signals and slots
+        self.contentListWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.contentListWidget.itemDoubleClicked.connect(self.onContentListWidgetItemDoubleClicked)
         self.contentListWidget.deletedAction.connect(self.onDeletePushButtonClicked)
         self.contentListWidget.activedAction.connect(self.onActivePushButtonClicked)
@@ -80,15 +83,16 @@ class OrganizedPlay(QDialog, ui_organizedplaydialog.Ui_organizedPlayDialog):
     def onEditingPushButtonClicked(self):
         e = EditingAction()
         e.exec_()
-    def onContentListWidgetItemDoubleClicked(self, widget):
-        widget.setFlags(widget.flags() | Qt.ItemIsEditable)
+    def onContentListWidgetItemDoubleClicked(self, widget): pass
+        # widget.setFlags(widget.flags() | Qt.ItemIsEditable)
+
     def onNewPushButtonClicked(self):
         new = NewAction()
         new.exec_()
     def onRenamePushButtonClicked(self):
         widget = self.contentListWidget.currentItem()
         if widget is not None:
-            widget.setFlags(widget.flags() | Qt.ItemIsEditable)
+            self.contentListWidget.editItem(widget)
     def onDeletePushButtonClicked(self):
         self.tipsLabel.setText(getFunctionName())
     def onExportPushButtonClicked(self):
